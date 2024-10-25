e2b53f96a4996a83dc483f92a7bc93af7423b8e7
galfansyahn <33422583+galfansyahn@users.noreply.github.com>
2024-10-25 09:50:46 +0700

    Tugas - 21 Oktober 2024
    Membuat aplikasi dengan total 5 activity menggunakan intent

---
+++ newnews/java/NewsUpload.java
@@ -0,0 +1,14 @@
+package com.example.newnews;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewsUpload extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+    }
+}
+
